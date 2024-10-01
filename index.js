a4a1ba5ec99414b372f2e8bb486091f73c727ba1
Aleks Lukin <al.lykin@mail.ru>
2024-10-01 19:42:00 +0300

    Start commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// const userAge = 29;
+// const userAge = 16;
+
+// if (userAge > 18) {
+//     console.log('Пользователю больше 18 лет');
+// } else if (userAge == 20) {
+//     console.log ('Пользователю 20 лет');
+// } else {
+//     console.log('Пользователю меньше 18 лет');
+
+// -----------
+
+// const userAge = 18;
+
+// if (userAge >= 18) {
+//     console.log('Пользователю больше или равно 18 лет');
+// } else {
+//     console.log('Пользователю меньше 18 лет');
+// }const userAge = 18;
+
+// if (userAge >= 18) {
+//     console.log('Пользователю больше или равно 18 лет');
+// } else {
+//     console.log('Пользователю меньше 18 лет');
+// }
+// ---------------
+// let temperature = -21;
+
+// if (temperature >-30) {
+//     console. log ('Сегодня холодно')
+// } 
+// if (temperature <=-20) {
+//     console. log ('Сегодня холодно')
+// }
+
+// ---------
+
+// for (let i : number = 0; i < 10; i++) {
+//     console. log('Я строка')
+// }
+// ------------------
+// let n = 100;
+// let m = 5;
+// let c = 0;
+// while (n>m){
+//     c = c + 1;
+//     n = n / 2;
+// }
+// console.log(c);
+// ------------
+
+// let c = 0;
+// for(let i = 1800; i<=2020; i++){
+//     c = c + 1;
+//     if (i === 1961){
+// console.log(c);
+//     }   
+// }
+// --------
+
+// let c = 0;
+// let cc = 0;
+// for(let i = 1800; i<=2020; i++){
+//     c = c + 1;
+//     if (i === 1961){
+// console.log(c);
+//     }   
+//     if (i%4===0){
+//         cc++;
+//     }
+// }
+// console.log(cc);
+// ---------------
